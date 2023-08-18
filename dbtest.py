6c4ae60ca29e01de08be5bf89b394e4573069cb7
Chip Cox <chip.cox@coxdogs.com>
2023-08-18 17:20:46 -0500

    vscode

--- dbtest.py
+++ dbtest.py
@@ -3,8 +3,24 @@
 ######
 import adbase
 import datetime
+import mysql.connector
 
 class dbtest(adbase.ADBase):
     def initialize(self):
         self.adapi = self.get_ad_api()
         self.adapi.log("Initializing %s",self.name)
+        
+        mydb=mysql.connector.connect(
+                host="192.168.2.20",
+                user="homeassistant",
+                password="Mario!23",
+                database="House10611",auth_plugin='mysql_native_password')
+
+        mycursor=mydb.cursor()
+        mycursor.execute("SELECT * FROM House10611.rooms order by mod(breaker,2)*-1,breaker,subbreaker")
+
+        myresult=mycursor.fetchall()
+
+        for x in myresult:
+            print(x)
+
